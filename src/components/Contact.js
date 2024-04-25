49961f7909435b0df68b23d9cf1a758be6a0b706
Nikshep A Kulli <48430792+nikshepkulli@users.noreply.github.com>
2024-04-25 10:15:37 -0400

    4/25/24 made few enhancement, which were missed...

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,16 +6,28 @@ const Contact = () => (
     {/* <h2>Contact</h2> */}
     <div className="cont">
       <div className="con1">
-        <p><a href="mailto:nikshepkulli9@gmail.com"><i className="fa fa-envelope" aria-hidden="true"></i></a></p>
+        <p>
+          <a href="mailto:nikshepkulli9@gmail.com">
+            <i className="fa fa-envelope" aria-hidden="true"></i>
+          </a>
+        </p>
       </div>
       {/* <div className="con2">
         <p><a href="tel:+1-203-220-1705"><i class="fa fa-phone" aria-hidden="true"></i></a></p>
       </div> */}
       <div className="con3">
-        <p><a href="https://www.linkedin.com/in/nikshepkulli/"><i class="fab fa-linkedin" aria-hidden="true"></i></a></p>
+        <p>
+          <a href="https://www.linkedin.com/in/nikshepkulli/">
+            <i class="fab fa-linkedin" aria-hidden="true"></i>
+          </a>
+        </p>
       </div>
       <div className="con3">
-        <p><a href="https://github.com/nikshepkulli"><i class="fab fa-github" aria-hidden="true"></i></a></p>
+        <p>
+          <a href="https://github.com/nikshepkulli">
+            <i class="fab fa-github" aria-hidden="true"></i>
+          </a>
+        </p>
       </div>
       {/* <div className="con4">
         <p><a href="https://maps.app.goo.gl/GLsyy1mhuoRJzq9G9"><i class="fa fa-home" aria-hidden="true"></i></a></p>
